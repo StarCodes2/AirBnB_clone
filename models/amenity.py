757a4486f1407e674d198f22ff3ad1e019f04568
Blessing <nejo.blessing@yahoo.com>
2023-12-09 03:17:28 -0800

    Airbnb Console

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The script defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """This represents and amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+
+    name = ""
